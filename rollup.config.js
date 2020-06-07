c6701211bf85af65d363cf1e4c88bba59ba54acd
boston <bost56@gmail.com>
2020-06-07 23:26:40 +0300

    up

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import builtins from 'rollup-plugin-node-builtins';
+import builtins from '@stream-io/rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
 import { terser } from "rollup-plugin-terser";
 
